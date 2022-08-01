41ff50d338c018e02048b222185980c0f473de09
Marouane Ahal <d144013152@gmail.com>
2022-08-01 19:40:19 +0100

    DataJPATest

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,19 +1,28 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
-    void itShouldCheckIfStudentExistsEmail() {
+    void itShouldCheckWhenStudentEmailExists() {
         // given
         String email = "jamila@gmail.com";
         Student student = new Student("Jamila", email, Gender.FEMALE);
@@ -25,4 +34,27 @@ class StudentRepositoryTest {
         // then
         assertThat(exists).isTrue();
     }
+
+
+    @Test
+    void itShouldCheckWhenStudentEmailDoesNotExists() {
+        // given
+        String email = "jamila@gmail.com";
+
+        // when
+        boolean exists = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(exists).isFalse();
+    }
+
+
 }
+
+
+
+
+
+
+
+
