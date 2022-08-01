87491af834eedd2e1887c20319a034c685e7926e
Marouane Ahal <d144013152@gmail.com>
2022-08-01 13:23:24 +0100

    test the numbers addition

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		// when
+		int result = underTest.add(numberOne, numberTwo);
+
+		// then
+		int expected = 50;
+		Assertions.assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
 	}
 
 }
